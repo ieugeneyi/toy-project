a7b85e8be84659fc5a6498c0a890d55aa5db65a7
ieugeneyi <ieugeneyi@gmail.com>
2022-07-21 11:14:52 +0900

    initial commit

---
+++ src/test/java/toy/study/ToyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package toy.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
